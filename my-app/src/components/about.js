cad11b7d99fc7039d217a36f76466283dd0154a5
markrh1201 <90006261+markrh1201@users.noreply.github.com>
2023-10-08 19:36:30 -0400

    about page added

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -1,13 +1,49 @@
-import React from "react";
-const About = () => {
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import "./about.css"
+import MarkPic from '../images/Mark Picture.jpg'
 
+const employees = [
+  {
+    name: 'Mark Howard',
+    position: 'CFO',
+    photo: MarkPic,
+    about: 'Mark is a senior majoring in Computer Science and Engineering at The Ohio State University. Mark was born and raised in Bedford, Ohio.',
+  },
+  {
+    name: 'Employee 2',
+    position: 'Position 2',
+    photo: MarkPic,
+    about: 'About Employee 2...',
+  },
+  {
+    name: 'Employee 3',
+    position: 'Position 3',
+    photo: MarkPic,
+    about: 'About Employee 3...',
+  }
+];
+
+const About= () => {
   return (
-    <div>
-      <h1>
-        about page
-      </h1>
-    </div>
+    <Container>
+      <h1>About Our Team</h1>
+      <Row>
+        {employees.map((employee, index) => (
+          <Col key={index} md={4}>
+            <Card  className="mb-3">
+              <Card.Img class = "img-responsive" variant="top" src={(employee.photo)}/>
+              <Card.Body>
+                <Card.Title className="employee-name">{employee.name}</Card.Title>
+                <Card.Subtitle className="position">{employee.position}</Card.Subtitle>
+                <Card.Text className="about-text">{employee.about}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
-  
+
 export default About;
