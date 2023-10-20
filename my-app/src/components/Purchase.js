7831e18ffe62bb40ec65e6493e4152840a5a411d
maxdaley <mdaley1@contactusinc.com>
2023-10-20 10:10:59 -0400

    Get inventory API

--- my-app/src/components/Purchase.js
+++ my-app/src/components/Purchase.js
@@ -1,69 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 const Purchase = () => {
 
     const location = useLocation();
     const navigate = useNavigate();
+    const [products, setProducts] = useState(null);
 
     //order is the value and setOrder is a function to update the value
     const [order, setOrder] = useState({
         buyQuantity: [0,0,0,0,0], credit_card_number: '', expir_date: '', cvvCode: '', 
         card_holder_name: '', address_1: '', address_2: '', city: '', state: '', zip: '',
     });
-    
+
+    useEffect(() => {
+        axios.get('https://3tcakfimxi.execute-api.us-east-2.amazonaws.com/prod/retrieve')
+            .then(response => {    
+                //setProducts(Object.entries(response.data.items));
+                setProducts(response.data.items)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
     const handleSubmit = (e) => {
         order.buyQuantity[e.currentTarget.value] += 1;
         navigate('/purchase/paymentEntry', {state: {order: order}})
     }
-
+    
+    
     return (
         <div className="container overflow-auto">
+            <div className="my-3"><input type="text" placeholder="Search" className="w-40"></input></div>
             <div className="container mt-2">
-                <div className="row d-flex justify-content-center gap-2">
-                    <div className="col-sm-5 d-flex border p-3">
-                        <img className="col-sm-6" src={require("../images/leopard.JPG")} alt="Leopard 2 Tank"></img>
-                        <div className="p-3 align-items-center w-100">
-                            <h4>
-                                Leopard 2 Tank
-                            </h4>
-                            <p>
-                                Cool Tank. Big Tank.
-                            </p>
-                            <button type="button" value={0} className="btn btn-primary mt-4" onClick={handleSubmit}>Purchase</button>
+                {products && Object.keys(products).map((itemId) => (
+                    <div className="row d-flex justify-content-center gap-2">
+                        {console.log("value: " + products[itemId])}
+                        <div className="col-sm-5 d-flex border p-3">
+                            <img className="col-sm-6" src={require(`../images/${products[itemId].image}`)} alt="Leopard 2 Tank"></img>
+                            <div className="p-3 align-items-center w-100">
+                                <h4>
+                                    {products[itemId].title}
+                                    {console.log(typeof(products[itemId].image))}
+                                </h4>
+                                <p>
+                                    {products[itemId].description}
+                                </p>
+                                <button type="button" value={0} className="btn btn-primary mt-4" onClick={handleSubmit}>Purchase</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                <div className="row d-flex justify-content-center gap-2">
-                    <div className="col-sm-5 d-flex border p-3">
-                        <img className="col-sm-6" src={require("../images/blackbird.jpg")} alt="Blackbird"></img>
-                        <div className="p-3 align-items-center w-100">
-                            <h4>
-                                Lockheed SR-71 Blackbird
-                            </h4>
-                            <p>
-                                Fastest plane
-                            </p>
-                            <button type="button" value={1} className="btn btn-primary mt-4" onClick={handleSubmit}>Purchase</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row d-flex justify-content-center gap-2">
-                    <div className="col-sm-5 d-flex border p-3">
-                        <img className="col-sm-6" src={require("../images/soltamk6.jpg")} alt="Soltam K6"></img>
-                        <div className="p-3 align-items-center w-100">
-                            <h4>
-                                Soltam K6 Mortar
-                            </h4>
-                            <p>
-                                Mortar with great range and fire rate.
-                            </p>
-                            <button type="button" value={2} className="btn btn-primary mt-4" onClick={handleSubmit}>Purchase</button>
-                        </div>
-                    </div>
-                </div>
+                ))}
+               
             </div>
             
             <ul className="pagination col-2-6 d-flex justify-content-center pt-3">
@@ -83,26 +73,7 @@ const Purchase = () => {
                     <a className="page-link" href="#">Next</a>
                 </li>
             </ul>
-            {/* when form is submitted handleSubmit will trigger and nav to payEntry */}
-            {/* <form onSubmit={handleSubmit}>
-                <label>Product 1</label>
-                <input
-                    type="number"
-                    required
-                    onChange={(e) =>
-                        {order.buyQuantity[0] = e.target.value;}}
-                />
-                <br/>
-                <label>Product 2</label> 
-                <input
-                    type="number"
-                    required
-                    onChange={(e) =>
-                        {order.buyQuantity[1] = e.target.value;}}
-                />
-                <br/>
-                <button className="button">Pay</button>
-                </form> */}
+            
         </div>
     );
 };
